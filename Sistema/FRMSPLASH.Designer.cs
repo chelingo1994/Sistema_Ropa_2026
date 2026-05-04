ed0bd580c6b192ecb27a447740f88dafb5558b18
HP <HP@DESKTOP-PCI93J0>
2026-05-04 16:29:19 -0400

    Agregar archivos de proyecto.

---
+++ Sistema/FRMSPLASH.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Sistema
+{
+    partial class FRMSPLASH
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRMSPLASH));
+            this.FRMRELOJ = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // FRMRELOJ
+            // 
+            this.FRMRELOJ.Tick += new System.EventHandler(this.FRMRELOJ_Tick);
+            // 
+            // FRMSPLASH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(379, 165);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FRMSPLASH";
+            this.Opacity = 0D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FRMSPLASH";
+            this.Load += new System.EventHandler(this.FRMSPLASH_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer FRMRELOJ;
+    }
+}
---
+++ Sistema/FRMSPLASH.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema
+{
+    public partial class FRMSPLASH : Form
+    {
+        bool bandera = false;
+        public FRMSPLASH()
+        {
+            InitializeComponent();
+        }
+
+        private void FRMRELOJ_Tick(object sender, EventArgs e)
+        {
+            if (!bandera && this.Opacity < 1)
+            {
+                this.Opacity += 0.05;
+            }
+            if ( this.Opacity==1)
+            {
+                bandera = true;
+            }
+            if (bandera && this.Opacity>0)
+            {
+                this.Opacity -= 0.05;
+            }
+            if (bandera && this.Opacity==0)
+            {
+                this.Close();
+            }
+        }
+
+        private void FRMSPLASH_Load(object sender, EventArgs e)
+        {
+          FRMRELOJ.Enabled = true;
+        }
+    }
+}
